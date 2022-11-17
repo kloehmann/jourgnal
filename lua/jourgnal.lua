e644af0c3b2927194f08158c796b8f7190255c21
Kevin Löhmann <kevin.loehmann@me.com>
2022-11-18 00:49:40 +0100

    code cleanup

--- lua/jourgnal.lua
+++ lua/jourgnal.lua
@@ -2,32 +2,12 @@
 local utils = require('jourgnal.utils')
 local conf = require('jourgnal.config')
 local tasks = require('jourgnal.tasks')
+local inbox = require('jourgnal.inbox')
 
 local M = { }
 
-function M.setup(config)
-  conf.init(config)
-end
-
--- TODO move this into a module and create a wrapper function 
-function M.add_task_to_inbox()
-  local task_title = ''
-  repeat
-    task_title = vim.fn.input('Task title: ')
-    print('')
-  until (task_title ~= '')
-  local task_line = '- [ ]'
-  task_line = utils.concat_if_not_empty(task_line, conf.default_status(), ' ')
-  task_line = utils.concat_if_not_empty(task_line, task_title, ' ')
-  task_line = utils.concat_if_not_empty(task_line, conf.default_when(), ' ')
-  task_line = utils.concat_if_not_empty(task_line, conf.default_deadline(), ' ')
-  task_line = utils.concat_if_not_empty(task_line, conf.default_tags(), ' ')
-  utils.append_to_file(conf.inbox_file(), task_line)
-end
-
-
-function M.toggle_task()
-  tasks.toggle_tickmark()
-end
+M.setup = function(config) conf.init(config) end
+M.add_task_to_inbox = function() inbox.add_task_to_inbox() end
+M.toggle_task = function() tasks.toggle_tickmark() end
 
 return M
