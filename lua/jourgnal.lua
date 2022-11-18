a651fa32a6851f37c9978f2cbea58c28d02b21a9
Kevin Löhmann <kevin.loehmann@me.com>
2022-11-18 08:44:54 +0100

    added insert task here

--- lua/jourgnal.lua
+++ lua/jourgnal.lua
@@ -2,12 +2,12 @@
 local utils = require('jourgnal.utils')
 local conf = require('jourgnal.config')
 local tasks = require('jourgnal.tasks')
-local inbox = require('jourgnal.inbox')
 
 local M = { }
 
 M.setup = function(config) conf.init(config) end
-M.add_task_to_inbox = function() inbox.add_task_to_inbox() end
+M.add_task_to_inbox = function() tasks.add_task_to_inbox() end
 M.toggle_task = function() tasks.toggle_tickmark() end
+M.add_task_here = function() tasks.add_task_here() end
 
 return M
--- lua/jourgnal/tasks.lua
+++ lua/jourgnal/tasks.lua
@@ -1,5 +1,7 @@
 -- functions for handling tasks
 local conf = require('jourgnal.config')
+local utils = require('jourgnal.utils')
+local fn = vim.fn
 -- local functions
 
 local match_pattern = '^.*- %[(.)%].*$'
@@ -9,6 +11,20 @@ local tick_val = function(line) return string.match(line, match_pattern) end
 local tick_task = function(line) return string.gsub(line, sub_pattern, '%1' .. conf.task_tick_char() .. '%3') end
 local untick_task = function(line) return string.gsub(line, sub_pattern, '%1 %3') end
 
+local function task_input()
+  local task_title = ''
+  repeat
+    task_title = fn.input('Task title: ')
+    print('')
+  until (task_title ~= '')
+  local task_line = '- [ ]'
+  task_line = utils.concat_if_not_empty(task_line, conf.default_status(), ' ')
+  task_line = utils.concat_if_not_empty(task_line, task_title, ' ')
+  task_line = utils.concat_if_not_empty(task_line, conf.default_when(), ' ')
+  task_line = utils.concat_if_not_empty(task_line, conf.default_deadline(), ' ')
+  task_line = utils.concat_if_not_empty(task_line, conf.default_tags(), ' ')
+  return task_line
+end
 -- global functions
 M = {}
 
@@ -23,4 +39,16 @@ function M.toggle_tickmark()
   vim.api.nvim_set_current_line(line)
 end
 
+function M.add_task_to_inbox()
+  local task_line = task_input()
+  utils.append_to_file(conf.inbox_file(), task_line)
+end
+
+function M.add_task_here()
+  local task_line = task_input()
+  fn.append(fn.line('.')-1,task_line)
+  fn.cursor(fn.line('.')-1, fn.col('.'))
+end
+
 return M
+
